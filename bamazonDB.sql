f229a75bc5c5cb28efbeffecaaf7aeabcabd4f86
Smarty Pants <amcurrent@hotmail.com>
2019-04-17 21:27:59 -0500

    saving

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -24,46 +24,48 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 -- Dumb Bells--
-VALUES ("Dumb Bell 2.5lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 5lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 10lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 12.5lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 15lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 20lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 25lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 30lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 35lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 40lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 45lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 50lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 55lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 60lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 65lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 70lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 75lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 80lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 85lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 90lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 95lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Dumb Bell 100lb", "Health and Fitness", 10.99, 1000);
+ VALUES
+   ("Dumb Bell 2.5lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 5lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 10lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 12.5lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 15lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 20lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 25lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 30lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 35lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 40lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 45lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 50lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 55lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 60lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 65lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 70lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 75lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 80lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 85lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 90lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 95lb", "Health and Fitness", 10.99, 1000),
+   ("Dumb Bell 100lb", "Health and Fitness", 10.99, 1000),
 -- Exercise Bands --
-VALUES ("Exercise Bands 2.5lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Exercise Bands 5lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Exercise Bands 10lb", "Health and Fitness", 10.99, 1000);
-VALUES ("Exercise Bands 12.5lb", "Health and Fitness", 10.99, 1000);
+   ("Exercise Bands 2.5lb", "Health and Fitness", 10.99, 1000),
+   ("Exercise Bands 5lb", "Health and Fitness", 10.99, 1000),
+   ("Exercise Bands 10lb", "Health and Fitness", 10.99, 1000),
+   ("Exercise Bands 12.5lb", "Health and Fitness", 10.99, 1000),
 -- Nutritionals--
-VALUES ("Vitamin A capsules", "Health and Beauty", 10.99, 1000);
-VALUES ("Vitamin C capsules", "Health and Beauty", 10.99, 1000);
-VALUES ("Omega 3 fish oil capsules", "Health and Beauty", 10.99, 1000);
-VALUES ("Magnesium pills", "Health and Beauty", 10.99, 1000);
-VALUES ("Complete Multi-vitamin", "Health and Beauty", 10.99, 1000);
-VALUES ("Multi-Vitmain Gummy Bears--For Kids!", "Health and Beauty", 10.99, 1000);
-VALUES ("Vitamin A capsules", "Health and Beauty", 10.99, 1000);
-VALUES ("Biotin capsules", "Health and Beauty", 10.99, 1000);
-VALUES ("Vitamin C capsules", "Health and Beauty", 10.99, 1000);
-VALUES ("Co-Q 10 capsules-90 servings", "Health and Beauty", 96.99, 1000);
-VALUES ("Cal-Mag", "Health and Beauty", 12.99, 1000);
-VALUES ("Folic Acid", "Health and Beauty", 10.99, 1000);
-VALUES ("COD Liver Oil", "Health and Beauty", 17.99, 1000);
-VALUES ("Chelated Iron capsules", "Health and Beauty", 10.99, 1000);
+   ("Vitamin A capsules", "Health and Beauty", 10.99, 1000),
+   ("Vitamin C capsules", "Health and Beauty", 10.99, 1000),
+   ("Omega 3 fish oil capsules", "Health and Beauty", 10.99, 1000),
+   ("Magnesium pills", "Health and Beauty", 10.99, 1000),
+   ("Complete Multi-vitamin", "Health and Beauty", 10.99, 1000),
+   ("Multi-Vitmain Gummy Bears--For Kids!", "Health and Beauty", 10.99, 1000),
+   ("Vitamin A capsules", "Health and Beauty", 10.99, 1000),
+   ("Biotin capsules", "Health and Beauty", 10.99, 1000),
+   ("Vitamin C capsules", "Health and Beauty", 10.99, 1000),
+   ("Co-Q 10 capsules-90 servings", "Health and Beauty", 96.99, 1000),
+   ("Cal-Mag", "Health and Beauty", 12.99, 1000),
+   ("Folic Acid", "Health and Beauty", 10.99, 1000),
+   ("COD Liver Oil", "Health and Beauty", 17.99, 1000),
+   ("Chelated Iron capsules", "Health and Beauty", 10.99, 1000);
 
+SELECT * FROM products;
